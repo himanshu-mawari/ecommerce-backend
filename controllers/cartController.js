e5c3dbae1567d75e8755432c74b597dacf19671a
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-03-09 21:44:10 +0530

    feat:- added update cart and get cart api

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,6 @@
-import product from "../models/product.js";
+import Product from "../models/product.js";
 import createError from "../helpers/createError.js";
+import User from "../models/user.js";
 
 // Add item to user cart
 export const addCartItem = async (req, res, next) => {
@@ -8,13 +9,13 @@ export const addCartItem = async (req, res, next) => {
     const loggedInUserCart = req.user.cartData;
     const { productId, quantity = 1 } = req.body;
 
-    const productExist = await product.findById(productId);
+    const productExist = await Product.findById(productId);
     if (!productExist) {
       return next(createError(404, "Product not found"));
     }
 
     let existingItem = loggedInUserCart.find(
-      (item ) => item.product.toString() === productId,
+      (item) => item.product.toString() === productId,
     );
     if (!existingItem) {
       loggedInUserCart.push({
@@ -29,7 +30,72 @@ export const addCartItem = async (req, res, next) => {
 
     res.json({
       message: "Product added to cart",
-      data: loggedInUserCart
+      data: loggedInUserCart,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// View user cart
+export const getCart = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).populate("cartData.product");
+
+    const cart = user.cartData;
+
+    res.json({
+      message: "User cart",
+      data: cart,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update cart item quantity
+export const updateCart = async (req, res, next) => {
+  try {
+    const loggedInUser = req.user;
+    const { productId } = req.params;
+    const { quantity } = req.body;
+
+    const productExist = await Product.findById(productId);
+    if (!productExist) {
+      return next(createError(404, "Product doesn't exist"));
+    }
+
+    const existCart = loggedInUser.cartData.find(
+      (item) => item.product.toString() === productId,
+    );
+    console.log(existCart)
+    if (!existCart) {
+      return next(createError(404, "Product not found"));
+    }
+
+    if (isNaN(quantity) || quantity < 0) {
+      return next(createError(400, "Quantity must be an positive number"));
+    }
+
+    if (productExist.stockQuantity < quantity) {
+      return next(
+        createError(400, `Only ${productExist.stockQuantity} items available`),
+      );
+    }
+
+    if (quantity === 0) {
+      loggedInUser.cartData = loggedInUser.cartData.filter(
+        (item) => item.product.toString() !== productId,
+      );
+    } else {
+      existCart.quantity = quantity;
+    }
+
+    await loggedInUser.save();
+
+    res.json({
+      message: "Successfully updated cart",
+      data: loggedInUser.cartData,
     });
   } catch (err) {
     next(err);
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { addCartItem } from "../controllers/cartController.js";
+import { addCartItem , getCart , updateCart } from "../controllers/cartController.js";
 import verifyAuth from "../middlewares/verifyAuth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.post("/add" , verifyAuth , addCartItem)
-
-
-
+cartRouter.post("/add" , verifyAuth , addCartItem);
+cartRouter.get("/" , verifyAuth , getCart);
+cartRouter.post("/update/:productId" , verifyAuth , updateCart);
 
 export default cartRouter;
