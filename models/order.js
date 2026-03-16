c016fb0697399057697bfde53f070e59fb824e0f
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-03-16 17:45:13 +0530

    feat: add order model

---
+++ models/order.js
@@ -0,0 +1,101 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        size: {
+          type: String,
+          enum: {
+            values: ["S", "M", "L", "XL", "XXL"],
+            message: `{VALUE} is not a valid size`,
+          },
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+    ],
+    shippingAddress: {
+      name: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["pending", "confirmed", "shipped", "delivered", "cancelled"],
+        message: `{VALUE} is not a valid order status`,
+      },
+      default: "pending",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
