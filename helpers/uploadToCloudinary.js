ada0cfdd7730403d22434584db391c8031584eea
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-09-05 19:05:50 +0530

    feat: add uploadToCloudinary helper for buffer-based uploads

---
+++ helpers/uploadToCloudinary.js
@@ -0,0 +1,16 @@
+import { v2 as cloudinary } from "cloudinary";  
+
+const uploadToCloudinary = (buffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder },
+      (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      }
+    );
+    stream.end(buffer);
+  });
+};
+
+export default uploadToCloudinary;
